97fa1fc54b2b1145c1cb5a14d288db5be47158a4
Marilyn <marilynromero16@gmail.com>
2021-12-09 04:54:15 -0300

    inicio proyecto

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+
+module.exports = {
+ 
+    mode: 'development',
+    output:{
+        clean: true
+    },
+    
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                sources: false,
+                }
+            },
+            {
+                test: /\.css$/i,
+                exclude: /styles.css$/,
+                use: [ 'style-loader','css-loader']
+            },
+            {
+                test: /styles.css$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'assets/img/[name].[ext]',
+                },
+            },
+
+
+            ]
+    },
+
+
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            inject: 'body'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets/'}
+            ]
+        })
+   
+    ]
+ 
+}
